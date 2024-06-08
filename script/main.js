5f422d40a8b93fdc36ddde3805d51d3fe90d5f8f
Mohamed Amine Chennadi <mochenna@e1r7p2.1337.ma>
2024-06-09 00:35:13 +0100

    home section is good still responsev code

--- script/main.js
+++ script/main.js
@@ -19,17 +19,17 @@ menu.onclick = () => {
     menu_active.classList.toggle("active-menu");
 }
 
-addEventListener("scroll", () => {
-    if (scol > window.scrollY)
-        document.body.children[0].classList.add("scroll-header");
-    else if (window.scrollY > scrol)
-        document.body.children[0].classList.remove("scroll-header");
-    scol = window.scrollY;
-    if (menu.classList.contains("btn-menu-active")) {
-        menu.classList.remove("btn-menu-active");
-        menu_active.classList.remove("active-menu");
-    }
-})
+// addEventListener("scroll", () => {
+//     if (scol > window.scrollY)
+//         document.body.children[0].classList.add("scroll-header");
+//     else if (window.scrollY > scol)
+//         document.body.children[0].classList.remove("scroll-header");
+//     scol = window.scrollY;
+//     if (menu.classList.contains("btn-menu-active")) {
+//         menu.classList.remove("btn-menu-active");
+//         menu_active.classList.remove("active-menu");
+//     }
+// })
 // item select click event add  option in home section 
 item_select_1.parentElement.onclick = () => {
     console.log("hey");
